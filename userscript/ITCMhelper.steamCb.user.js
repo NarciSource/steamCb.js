80372282b304279801396130bbd788f47c804ded
narci nar <jwch11@gmail.com>
2019-02-04 01:43:06 +0900

    Change source directory

--- userscript/ITCMhelper.steamCb.user.js
+++ userscript/ITCMhelper.steamCb.user.js
@@ -8,13 +8,13 @@
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
 // @require      http://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.1/js/jquery.tablesorter.min.js
-// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/require.js
-// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/magicsuggest.js
+// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/vendor/require.js
+// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/vendor/magicsuggest.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.2/ace.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.2/mode-css.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.2/mode-json.js
-// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/ginfoBuilder.js
-// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/steamCb.js
+// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/src/ginfoBuilder.js
+// @require      https://raw.githubusercontent.com/NarciSource/steamCb.js/master/src/steamCb.js
 // @resource     popup-layout https://raw.githubusercontent.com/NarciSource/steamCb.js/master/html/popup.html
 // @resource     side-layout https://raw.githubusercontent.com/NarciSource/steamCb.js/master/html/side.html
 // @resource     cb-style https://raw.githubusercontent.com/NarciSource/steamCb.js/master/css/cb.default.css
